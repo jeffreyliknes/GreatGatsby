ed9b5404c2bbd9f3b1c26817826e4baedcf02d04
Jeff Liknes <jeff.liknes@gmail.com>
2019-09-03 16:18:19 -0600

    netlify addition

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -9,6 +9,7 @@ exports.components = {
   "component---src-templates-page-js": hot(preferDefault(require("/Applications/MAMP/htdocs/proportfolio/src/templates/page.js"))),
   "component---src-templates-portfolio-js": hot(preferDefault(require("/Applications/MAMP/htdocs/proportfolio/src/templates/portfolio.js"))),
   "component---src-templates-blog-post-list-js": hot(preferDefault(require("/Applications/MAMP/htdocs/proportfolio/src/templates/blogPostList.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Applications/MAMP/htdocs/proportfolio/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Applications/MAMP/htdocs/proportfolio/src/pages/404.js")))
 }
 
