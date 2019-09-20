604f68eb1b5e5f4bb87d9723366ef2bab5b40f34
Jeff Liknes <jeff.liknes@gmail.com>
2019-09-19 21:41:26 -0600

    redirect to home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -119,7 +119,8 @@ module.exports = {
           "**/*/*/users",
           "**/*/*/menus",
           "**/*/*/portfolio",
-          
+          "**/*/*/logo",
+          "**/*/*/favicon",
         ],
         // Blacklisted routes using glob patterns
         excludedRoutes: [],
