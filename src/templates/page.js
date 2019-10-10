13a4243de8876240d9d4c578c5c521510d294c1f
Jeff Liknes <jeff.liknes@gmail.com>
2019-10-10 12:14:18 -0600

    many changes done to layout and design

--- src/templates/page.js
+++ src/templates/page.js
@@ -3,28 +3,28 @@ import Layout from "../components/layout";
 import "../components/styles/contentStyle.scss";
 import "../components/styles/logoStyle.scss";
 import BackgroundImage from "../components/homeImage";
-import styled from "styled-components";
+// import styled from "styled-components";
 import SiteInfo from "../components/siteInfo";
 
 
-const FeaturedImage = styled.img`
-  max-width: 300px;
-  margin: 16px 0;
-`;
+// const FeaturedImage = styled.img`
+//   max-width: 300px;
+//   margin: 16px 0;
+// `;
 
-const FeaturedLogo = styled.div`
-display: flex;
-justify-content: center;
-`;
+// const FeaturedLogo = styled.div`
+// display: flex;
+// justify-content: center;
+// `;
 
 export default ({ pageContext, data }) => (
   <Layout>
     <SiteInfo />
     <h1 dangerouslySetInnerHTML={{ __html: pageContext.title }} />
     <div dangerouslySetInnerHTML={{ __html: pageContext.content }} />
-    <FeaturedLogo>
+    {/* <FeaturedLogo>
       <FeaturedImage src={pageContext.featured_media.source_url} />
-    </FeaturedLogo>
+    </FeaturedLogo> */}
     <BackgroundImage />
   </Layout>
 );
